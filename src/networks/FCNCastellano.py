63eee7166deb1ca00221c25e526add41a131f6f1
olega <and.olega@gmail.com>
2020-10-27 22:55:00 +0300

    + add lightning support ! change image flatten

--- src/networks/FCNCastellano.py
+++ src/networks/FCNCastellano.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from pytorch_lightning.core.lightning import LightningModule
+
 
 class FCNCastellano(nn.Module):
     def __init__(self, in_channels=3):
@@ -34,7 +36,23 @@ class FCNCastellano(nn.Module):
 
     def forward(self, x):
         x = self.features(x)
-        x = x.view(-1)  # TODO make a correct flatten
+        x = x.view(x.size(0), -1)
         x = self.head(x)
-
         return x
+
+
+class ExtendedFCNCastellano(FCNCastellano, LightningModule):
+    def __init__(self):
+        super().__init__()
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        preds = self.forward(x.float())
+        preds = torch.reshape(preds, (-1,))
+        loss = F.l1_loss(preds, y.float())  # mae loss
+        self.log('train_loss', loss)
+        return {'loss': loss}
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(self.parameters(), momentum=0.9, lr=10e-5)
+        return optimizer
