700a520beaa34d8b91ef3742ea940969daf21e82
olega <and.olega@gmail.com>
2020-10-27 22:59:27 +0300

    + add dataset info console output

---
+++ src/utils/print_info.py
@@ -0,0 +1,15 @@
+from torch.utils.data import Dataset, DataLoader
+
+
+def print_dataset_info(dataset: Dataset,
+                       dataloader: DataLoader,
+                       name: str = 'train') -> None:
+    """
+    Prints dataset, dataloader info to console
+    :param dataset: PyTorch Dataset
+    :param dataloader: PyTorch DataLoader
+    :param name: Dataset identity (train/test/val/etc.)
+    """
+    print(f'- Dataset {name} is loaded')
+    print(f' - Loaded train len: {len(dataset)}')
+    print(f' - Loaded train batches len: {len(dataloader)}')
