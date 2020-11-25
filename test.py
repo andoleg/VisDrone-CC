bc5e1a5052092f2e6178e49618def8b465cc406a
olega <and.olega@gmail.com>
2020-11-25 14:04:25 +0300

    + add dataset support (test) to config

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ from src.data import VisDroneDatasetCC
 from src.data import visdrone_read_train_test, train_val_split
 from src.networks.FCNCastellano import FCNCastellano, ExtendedFCNCastellano
 from src.utils.print_info import print_dataset_info
-from src.config import TrainerConfig
+from src.config import TrainerConfig, VisDroneDataConfig
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -23,19 +23,21 @@ if __name__ == '__main__':
     config_yaml = yaml.load(open(config_path, "r"), Loader=yaml.FullLoader)
 
     # Load data
-    dataset_root = Path(config_yaml['data']['dataset_root'])
-    train_split, test_split = visdrone_read_train_test(data_root=dataset_root)
+    dataset_params = VisDroneDataConfig(**config_yaml['data'])
+    dataset_params.data_root = Path(dataset_params.data_root)
+    # dataset_root = Path(config_yaml['data']['dataset_root'])
+    train_split, test_split = visdrone_read_train_test(data_root=dataset_params.data_root)
     train_split, val_split = train_val_split(train_split)
 
-    train_dataset = VisDroneDatasetCC(train_split, data_root=dataset_root)
-    val_dataset = VisDroneDatasetCC(val_split, data_root=dataset_root)
-    test_dataset = VisDroneDatasetCC(test_split, data_root=dataset_root, resize=(512, 512), train=False)
+    train_dataset = VisDroneDatasetCC(train_split, **dataset_params.dict())
+    val_dataset = VisDroneDatasetCC(val_split, **dataset_params.dict())
+    # test_dataset = VisDroneDatasetCC(test_split, data_root=dataset_root, resize=(512, 512), train=False)
     train_dataloader = DataLoader(train_dataset, batch_size=64, num_workers=5, shuffle=True, )
     val_dataloader = DataLoader(val_dataset, batch_size=64, num_workers=5)
-    test_dataloader = DataLoader(train_dataset, batch_size=64, num_workers=5)
+    # test_dataloader = DataLoader(train_dataset, batch_size=64, num_workers=5)
 
     # Dataset info
-    print_dataset_info(train_dataset, train_dataloader)
+    # print_dataset_info(train_dataset, train_dataloader)
     print_dataset_info(val_dataset, val_dataloader, name='val')
 
     # Load model
