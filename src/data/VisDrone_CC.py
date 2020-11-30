b8d2295ce0734749a183f3cd30ad1aaf6422f62d
olega <and.olega@gmail.com>
2020-11-30 11:12:27 +0300

    + add discrete map option to dataset

--- src/data/VisDrone_CC.py
+++ src/data/VisDrone_CC.py
@@ -1,8 +1,9 @@
 import cv2
 from torch.utils.data import Dataset
-from collections import Counter
-
+from collections import Counter, defaultdict
 from pathlib import Path
+
+from src.data.utils import gen_discrete_map
 
 
 class VisDroneDatasetCC(Dataset):
@@ -11,7 +12,8 @@ class VisDroneDatasetCC(Dataset):
                  an_folders: str = 'annotations',
                  resize: tuple = (128, 128),
                  normalize: bool = True,
-                 train: bool = True) -> None:
+                 train: bool = True,
+                 n_points: bool = True) -> None:
         """
         :param folder_ids: list of ids of dataset tracks
         :param data_root: path to root "VisDrone2020-CC" folder
@@ -20,11 +22,13 @@ class VisDroneDatasetCC(Dataset):
         :param resize: desired image size
         :param normalize: image normalization
         :param train: if false, uses test data (that has no annotations)
+        :param n_points: if true will use number of people as label, otherwise generate density map
         """
         self.img_paths = list()  # list of tuples: (image_path, people_count)
         self.resize = resize
         self.normalize = normalize
         self.train = train
+        self.n_points = n_points
 
         data_root = Path(data_root)
         if self.train:
@@ -46,11 +50,14 @@ class VisDroneDatasetCC(Dataset):
         image_path, label = self.img_paths[item]
 
         image = cv2.imread(str(image_path))
+        in_shape = image.shape[:2]
         image = cv2.resize(image, self.resize)
         if self.normalize:
             image = image / 255.0
 
         image = image.transpose(2, 0, 1)
+        if not self.n_points:
+            label = gen_discrete_map(in_shape, label, tuple(x // 2 - 8 for x in self.resize))
 
         if self.train:
             return image, label
@@ -63,16 +70,25 @@ class VisDroneDatasetCC(Dataset):
     def __str__(self):
         return [x[0] for x in self.img_paths]
 
-    @staticmethod
-    def read_annotation_file(filename: Path) -> dict:
+    def read_annotation_file(self, filename: Path) -> dict:
         """
         :param filename: path to annotation file
+        :param return_points: return # of points or list of points (True for list)
         :return: dict {'image_id': # of people}
         """
-        with filename.open() as file:
-            annotations = [x.split(',') for x in file.read().split()]
+        if self.n_points:
+            with filename.open() as file:
+                annotations = [x.split(',') for x in file.read().split()]
 
-        return Counter([x[0] for x in annotations])
+            return Counter([x[0] for x in annotations])
+        else:
+            with filename.open() as file:
+                annotations = [x.split(',') for x in file.read().split()]
+
+            points = defaultdict(list)
+            for point in annotations:
+                points[point[0]].append((int(point[1]), int(point[2])))
+            return points
 
 
 # a = VisDroneDatasetCC(['00001'], '/Users/olega/Downloads/VisDrone2020-CC', resize=(700, 700))
--- src/data/utils.py
+++ src/data/utils.py
@@ -1,7 +1,8 @@
+import numpy as np
 from pathlib import Path
 from torch import Generator
 from torch.utils.data import random_split
-
+from cv2 import resize, INTER_CUBIC
 
 def visdrone_read_train_test(data_root: Path,
                              testlist_name: str = 'testlist.txt',
@@ -31,3 +32,36 @@ def train_val_split(dataset: list, train_size: float = 0.9) -> (list, list):
                                           generator=Generator().manual_seed(42))
 
     return train_split, val_split
+
+
+def gen_discrete_map(im_size, points, resize_shape=None):
+    """
+    Generate the discrete map.
+    :param points: [num_gt, 2]
+    :return:
+    """
+    points = np.array(points)
+    discrete_map = np.zeros(im_size, dtype=np.float32)
+    h, w = discrete_map.shape[:2]
+    num_gt = points.shape[0]
+    if num_gt == 0:
+        return discrete_map
+    for p in points:
+        p = np.round(p).astype(int)
+        p[0], p[1] = min(h - 1, p[1]), min(w - 1, p[0])
+        discrete_map[p[0], p[1]] += 1
+
+    # print(f'W: {w}, H: {h}, map: {discrete_map.shape}')
+    if type(resize_shape) is tuple:
+        # discrete_map = np.resize(discrete_map, resize)
+        # down_w = (w // 2) - 8
+        # down_h = (h // 2) - 8
+        # discrete_map = discrete_map.reshape([down_h, 2, down_w, 2]).sum(axis=(1, 3))
+        print(f'Before: {discrete_map.shape}')
+        discrete_map = resize(discrete_map, dsize=resize_shape)
+        print(f'After: {discrete_map.shape}, GT: {num_gt}, New: {np.sum(discrete_map)}')
+
+        # todo solve reshaping issue
+
+    assert np.sum(discrete_map) == num_gt
+    return discrete_map
