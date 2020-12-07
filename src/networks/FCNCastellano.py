2f6a890dc396442e536017b2bdd50648f71ec340
olega <and.olega@gmail.com>
2020-12-07 20:41:42 +0300

    = change train/val/test batch runs

--- src/networks/FCNCastellano.py
+++ src/networks/FCNCastellano.py
@@ -48,40 +48,45 @@ class ExtendedFCNCastellano(FCNCastellano, LightningModule):
         self.val_losses = list()
 
     def training_step(self, batch, batch_idx):
-        x, y = batch
-        preds = self.forward(x.float())
-        preds = torch.reshape(preds, (-1,))
-        loss = F.l1_loss(preds, y.float())  # mae loss
+        loss = self.run_batch(batch, batch_idx)
         self.train_losses.append(loss.item())
         self.log('train_loss', loss)
         return {'loss': loss}
 
     def training_epoch_end(self, outputs) -> None:
-        average_loss = np.mean(self.train_losses)
+        average_loss = torch.mean(torch.stack([x["loss"] for x in outputs]))
         self.log('train_epoch_loss', average_loss)  # log mean losses on epoch end
         self.train_losses = list()
 
     def validation_step(self, batch, batch_idx):
-        x, y = batch
-        preds = self.forward(x.float())
-        preds = torch.reshape(preds, (-1,))
-        loss = F.l1_loss(preds, y.float())  # mae loss
+        loss = self.run_batch(batch, batch_idx)
         metrics = {'val_loss': loss}
         self.val_losses.append(loss.item())
-        # self.log('val_loss', loss)
         self.log_dict(metrics)
         return metrics
 
     def validation_epoch_end(self, outputs) -> None:
-        average_loss = np.mean(self.val_losses)
+        average_loss = torch.mean(torch.stack([x["val_loss"] for x in outputs]))
         self.log('val_epoch_loss', average_loss)  # log mean losses on epoch end
         self.val_losses = list()
 
     def test_step(self, batch, batch_idx):
-        metrics = self.validation_step(batch, batch_idx)
-        metrics = {'test_loss': metrics['val_loss']}
-        self.log_dict(metrics)
+        loss = self.run_batch(batch, batch_idx)
+        metrics = {'test_loss': loss}
+        self.log_dict(metrics, on_step=True, on_epoch=True)
+        return metrics
+
+    def test_epoch_end(self, outputs) -> None:
+        average_loss = torch.mean(torch.stack([x["test_loss"] for x in outputs]))
+        self.log('test_loss_average', average_loss)
 
     def configure_optimizers(self):
         optimizer = torch.optim.SGD(self.parameters(), momentum=0.9, lr=10e-5)
         return optimizer
+
+    def run_batch(self, batch, batch_idx):
+        x, y = batch
+        preds = self.forward(x.float())
+        preds = torch.reshape(preds, (-1,))
+        loss = F.l1_loss(preds, y.float())  # mae loss
+        return loss
--- src/networks/bottleneck_exp.py
+++ src/networks/bottleneck_exp.py
@@ -59,10 +59,7 @@ class ExtendedFCNCastellanoBN(FCNCastellanoBN, LightningModule):
         self.val_losses = list()
 
     def training_step(self, batch, batch_idx):
-        x, y = batch
-        preds = self.forward(x.float())
-        preds = torch.reshape(preds, (-1,))
-        loss = F.l1_loss(preds, y.float())  # mae loss
+        loss = self.run_batch(batch, batch_idx)
         self.train_losses.append(loss.item())
         self.log('train_loss', loss)
         return {'loss': loss}
@@ -73,13 +70,9 @@ class ExtendedFCNCastellanoBN(FCNCastellanoBN, LightningModule):
         self.train_losses = list()
 
     def validation_step(self, batch, batch_idx):
-        x, y = batch
-        preds = self.forward(x.float())
-        preds = torch.reshape(preds, (-1,))
-        loss = F.l1_loss(preds, y.float())  # mae loss
+        loss = self.run_batch(batch, batch_idx)
         metrics = {'val_loss': loss}
         self.val_losses.append(loss.item())
-        # self.log('val_loss', loss)
         self.log_dict(metrics)
         return metrics
 
@@ -89,10 +82,22 @@ class ExtendedFCNCastellanoBN(FCNCastellanoBN, LightningModule):
         self.val_losses = list()
 
     def test_step(self, batch, batch_idx):
-        metrics = self.validation_step(batch, batch_idx)
-        metrics = {'test_loss': metrics['val_loss']}
-        self.log_dict(metrics)
+        loss = self.run_batch(batch, batch_idx)
+        metrics = {'test_loss': loss}
+        self.log_dict(metrics, on_step=True, on_epoch=True)
+        return metrics
+
+    def test_epoch_end(self, outputs) -> None:
+        average_loss = torch.mean(torch.stack([x["test_loss"] for x in outputs]))
+        self.log('test_loss_average', average_loss)
 
     def configure_optimizers(self):
         optimizer = torch.optim.SGD(self.parameters(), momentum=0.9, lr=10e-5)
         return optimizer
+
+    def run_batch(self, batch, batch_idx):
+        x, y = batch
+        preds = self.forward(x.float())
+        preds = torch.reshape(preds, (-1,))
+        loss = F.l1_loss(preds, y.float())  # mae loss
+        return loss
