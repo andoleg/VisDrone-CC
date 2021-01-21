2a76d4e0188224097c28f7b8cf1f64e370f44744
olega <and.olega@gmail.com>
2021-01-21 22:28:20 +0300

    + add init of efficientnet without pretrained weights

--- src/networks/EfficientNet/__init__.py
+++ src/networks/EfficientNet/__init__.py
@@ -7,16 +7,21 @@ from .utils import (
     get_model_params,
 )
 
-from  torch import nn
 from torch.nn import Module
 
 
 class EfficientNetModel(Module):
     def __init__(self, model_name, weights_path=None, advprop=False,
-                 in_channels=3, num_classes=1, **override_params):
+                 in_channels=3, num_classes=1, pretrained=True, **override_params):
         super().__init__()
-        self.model = EfficientNet.from_pretrained(model_name, weights_path, advprop=advprop,
-                                                  in_channels=in_channels, num_classes=num_classes, **override_params)
+
+        if pretrained:
+            self.model = EfficientNet.from_pretrained(model_name, weights_path, advprop=advprop,
+                                                      in_channels=in_channels, num_classes=num_classes,
+                                                      **override_params)
+        else:
+            self.model = EfficientNet.from_name(model_name, num_classes=num_classes, in_channels=in_channels,
+                                                **override_params)
 
     def forward(self, inputs):
         return self.model(inputs)
